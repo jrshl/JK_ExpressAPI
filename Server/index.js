aeb5215fa67c15a45fc9d65fdd894aae0cfd0ffe
jrshl <jershielcalamba@gmail.com>
2025-09-25 11:46:20 +0800

    New project files

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fetch = require('node-fetch'); // if Node <18, else remove this line
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static frontend files (index.html, css, images) from "public" folder
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+// API route to fetch cat facts
+app.get('/api/fact', async (req, res) => {
+  try {
+    const response = await fetch('https://meowfacts.herokuapp.com/');
+    const data = await response.json();
+    res.json({ fact: data.data[0] });
+  } catch (err) {
+    res.status(500).json({ fact: "Cats are amazing creatures!" });
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
